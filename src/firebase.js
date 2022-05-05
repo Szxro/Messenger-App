44c00faa66d06242f37c5fe0fc91ed5a4258d24d
Szxro <szxrocode@gmail.com>
2022-05-05 19:01:45 -0400

    update dependencys

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,12 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
-const firebaseapp=firebase.initializeApp({
-    apiKey: "AIzaSyC3R_wAY9K4P4SmwFzIsy77X14l_1Wt1lg",
-    authDomain: "firechat-c0518.firebaseapp.com",
-    projectId: "firechat-c0518",
-    storageBucket: "firechat-c0518.appspot.com",
-    messagingSenderId: "1085473703570",
-    appId: "1:1085473703570:web:f45ef9686533c9f68e084b"
-})
+const firebaseapp = firebase.initializeApp({
+  /*
+    YOUR FIREBASE KEYS
+    */
+});
 
-const db=firebaseapp.firestore()
-const auth=firebase.auth()
+const db = firebaseapp.firestore();
+const auth = firebase.auth();
 
-export {db,auth}
+export { db, auth };
